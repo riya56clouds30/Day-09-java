68a3990fbd516b4fe58bd61fee278ff8e5ba18bd
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-10 15:42:44 +0530

    Add files via upload

---
+++ togglingcase1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class togglingcase1{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char s=sc.next().charAt(0);
+        if((s&32)==0)
+        {
+            System.out.println("uppercase");
+        }
+            else
+        {
+           System.out.println("lowercase");                                 
+
+            }
+        }
+    }
+    
